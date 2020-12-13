f827d9c362d4b484fcee25f826b64bff9cd68a0c
unknown <luisrodrigojimenez@gmail.com>
2020-12-13 14:13:15 -0500

    texto y tabla colores

--- appnotas/src/App.svelte
+++ appnotas/src/App.svelte
@@ -52,7 +52,7 @@
   .contenedor {
     display: flex;
     height: 100px;
-    width: 600px;
+    width: 1000px;
     margin: 0 auto;
     justify-content: center;
     flex-flow: row wrap;
@@ -60,7 +60,7 @@
   }
 
   .titulo {
-    width: 600px;
+    width: 800px;
   }
 
   img {
@@ -77,20 +77,51 @@
 
   .verde {
     color: #2cd078;
-  }
+	}
+	
+	.azul {
+		background:#6697c8;
+		color: #fff;
+		
+	}
+
+	.cajaroja {
+		background:#ff0b0b;
+		color: #fff;
+	}
+
+	.cajanaranja {
+		background:#ff9900;
+		color: #fff;
+	}
+
+	.cajaverde {
+		background:#2cdd78;
+		color: #fff;
+	}
+
+	.gris {
+		background: #f0f0f0;
+		color: #a39d9d;
+	}
+
+	.blanco {
+		background: #feffe3;
+		color: #a39d9d;
+	}
 
   .grid-container {
     display: grid;
-    width: 600px;
-    grid-template-rows: 250px 250px;
-    grid-template-columns: 1fr 7fr;
+    width: 800px;
+    grid-template-rows: 200px 250px 200px;
+    grid-template-columns: 1fr 3fr 6fr;
 
-    grid-gap: 1px;
+    grid-gap: 15px;
   }
 
   .grid-1 {
     text-align: center;
-    grid-row: 1 / 2;
+    grid-row: 1 / 1;
     grid-gap: 5px;
   }
 
@@ -104,9 +135,9 @@
 
   form {
     display: grid;
-    grid-template-columns: 1fr 2fr;
+    grid-template-columns: 2fr 1fr;
     grid-gap: 5px;
-    padding: 20px 40px;
+    padding: 10px 30px;
   }
 
   form button {
@@ -119,25 +150,41 @@
     background: #6697c8;
     color: #feffe3;
     text-align: left;
-    padding: 20px;
+    padding: 10px;
   }
 
   .grid-3 {
     text-align: center;
-    border: #f0f0f0 1px solid;
-    grid-column: 1/3;
-    grid-gap: 20px;
-    padding: 20px;
+    border: #f0f0f0 1px;
+		grid-column: 1/3;
+		grid-row: 2/ 2;
+    grid-gap: 0px;
+    padding: 20px 0px;
+	}
+
+	.grid-4 {
+		border: #f0f0f0 1px solid;
+		background: #feffe3;
+		grid-row: 3/ 1;
+		grid-column: 3/3;
+		grid-gap: 20px;
+		padding: 10px;
+	}
+
+	.grid-5 {
+		border: #f0f0f0 1px;
+		grid-column: 3/3;
+		padding: 0px 20px;
 	}
 	
-	table {
+.notas {
 	 width: 100%;
 	 border: 1px solid #6697c8; 
 	 	border-collapse: collapse;
 	
 }
 
-table thead th {
+.notas thead th {
 	background: #6697c8;
 	color: #f0f0f0;
 	border: 1px solid #6697c8; 
@@ -146,20 +193,20 @@ table thead th {
 	font-weight: normal;
 	
 }
-table tbody td {
+
+.notas tbody td {
 	 width: 25%;
 		text-align: center;
 		border: 1px solid #6697c8;
-		
 		border-collapse: collapse;
 }
 
-.mat {
+.notas .mat {
 	text-align: left;
 	padding-left: 10px;
 }
 
-table tfoot th {
+.notas tfoot th {
 	background: #6697c8;
 	color: #f0f0f0;
 	text-align: right;
@@ -167,15 +214,35 @@ table tfoot th {
 	font-weight: normal;
 }
 
-table tfoot td {
+.notas tfoot td {
 	border: 1px solid #6697c8;
 	font-weight: bold;
 }
 
-table tbody td button {
+.notas tbody td button {
 	border: none;
 	background: none;
 	
+}
+
+li, ul {
+	list-style-type: none;
+	margin-top: 0px;
+	margin-bottom: 0px;
+	
+}
+
+.colores  {
+	width: 100%;
+	border: none;
+	border-collapse: collapse;
+	
+}
+
+.colores td {
+	text-align: center;
+	margin: 10px;
+	padding: 10px;
 }
 
 </style>
@@ -221,7 +288,7 @@ table tbody td button {
       </div>
 
       <div class="grid-3">
-        <table>
+        <table class="notas">
           <thead>
             <tr>
 
@@ -255,7 +322,7 @@ table tbody td button {
           </tbody>
           <tfoot>
             <tr>
-              <th>promedio</th>
+              <th>Promedio</th>
               {#if promedio <= 2.5}
                 <td class="rojo">{promedio}</td>
               {:else if promedio <= 2.9}
@@ -269,6 +336,38 @@ table tbody td button {
         </table>
 
       </div>
+
+			<div class="grid-4">
+				<li>- Al hacer click en el botón <strong>agregar</strong> se adiciona la nota a la tabla y recalcula el promedio de todas las notas.</li>
+				<li>- Al hacer click en el icono rojo al final de cada fila de la tabla debe eliminar la nota y recalcular el promedio</li>
+				<li>- Color de las notas y el prmedio:</li>
+				<ul><li>- Entre 0 y 2.5 = rojo</li>
+				<li>- Entre 2.6 y 2.9 = naranja</li>
+				<li>- Entre 3.0 y 5.0 = verde</li></ul>
+				<li>-Al agregar una nueva materia no debe permitir:</li>
+				<ul><li>- Agregar materias vacias</li>
+				<li>- Notas no numéricas</li>
+				<li>- Notas por fuera de 0 y 5.0</li></ul>
+				<li>- Se debe resperar los colores, alineación, fuentes, etc</li>
+				<li>- El título de la página debe tener el nombre del estudiante y su docimento de identidad.</li>
+				<li>- Esta nota debe presentarse en la pagina</li>
+				<li>- La entrega consiste en la URL del sitio publicado en Netlify</li>
+			</div>
+
+			<div class="grid-5">
+				<table class="colores">
+					<tr>
+						<td class="azul">#6697c8</td>
+						<td class="cajaroja">#FF0B0B</td>
+						<td class="gris">#F0F0F0</td>
+					</tr>
+					<tr>
+						<td class="cajanaranja">#FF9900</td>
+						<td class="cajaverde">#2CDD78</td>
+						<td class="blanco">#FEFFF3</td>
+					</tr>
+				</table>
+			</div>
     </div>
 
   </div>
